92c7b1ebf13d9df95599cfdb63f0bd1ad4cbe558
parkjusu3321 <jackpot3016@naver.com>
2024-02-22 16:06:44 +0900

    323134

--- dependencies/config.py
+++ dependencies/config.py
@@ -7,8 +7,8 @@ class DefaultConfig(BaseSettings):
     postgresql_endpoint: str = os.getenv("POSTGRESQL_ENDPOINT", "svc.sel4.cloudtype.app")
     postgresql_port: int = os.getenv("POSTGRESQL_PORT", "32752")
     postgresql_table: str = os.getenv("POSTGRESQL_TABLE", "reaction_db")
-    postgresql_user: str = os.getenv("POSTGRESQL_USER", "")
-    postgresql_password: str = os.getenv("POSTGRESQL_PASSWORD", "")
+    postgresql_user: str = os.getenv("POSTGRESQL_USER", "root")
+    postgresql_password: str = os.getenv("POSTGRESQL_PASSWORD", "3321")
 
 
 @lru_cache
