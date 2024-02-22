55bdf24a77da4798f64e8b688f8d4e05009028b8
parkjusu3321 <jackpot3016@naver.com>
2024-02-22 15:41:55 +0900

    123f

--- dependencies/config.py
+++ dependencies/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 
 class DefaultConfig(BaseSettings):
-    postgresql_endpoint: str = os.getenv("POSTGRESQL_ENDPOINT", "postgresql")
+    postgresql_endpoint: str = os.getenv("POSTGRESQL_ENDPOINT", "svc.sel4.cloudtype.app")
     postgresql_port: int = os.getenv("POSTGRESQL_PORT", "32752")
     postgresql_table: str = os.getenv("POSTGRESQL_TABLE", "reaction_db")
     postgresql_user: str = os.getenv("POSTGRESQL_USER", "")
