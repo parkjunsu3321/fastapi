f4805277cf81b5f5d6ad0e95091e468dd19c4f29
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-23 23:09:11 +0900

    Update repositories.py

--- domains/users/repositories.py
+++ domains/users/repositories.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from .models import UserModel
+from .models import GameResultModel
 
 class UserRepository:
     def __init__(self, session: AsyncSession):
