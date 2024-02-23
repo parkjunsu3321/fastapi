792021132bbc225d5ba04e20769aeefb74772e3a
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-23 19:10:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import logging
-import traceback
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from starlette.requests import Request
 from starlette.responses import JSONResponse
+from starlette.staticfiles import StaticFiles
+import logging
+import traceback
 
 from dependencies.database import init_db
 from dependencies.config import get_config
@@ -26,15 +27,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 정적 파일 제공
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 # 라우터 추가
 app.include_router(router=main_router)
 
 # 데이터베이스 초기화
 init_db(config=get_config())
-
-
-
-
 
 # 예외 처리
 @app.exception_handler(Exception)
@@ -42,6 +42,10 @@ async def exception_handler(request: Request, exc: Exception):
     logging.error(traceback.format_exc())
     return JSONResponse(status_code=500, content={"detail": "Internal server error"})
 
+# /docs, /redoc 리디렉션 처리
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     import uvicorn
