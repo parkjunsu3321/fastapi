e3204672be634d1ab166fffd5c03044ea9ce636f
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-23 22:08:10 +0900

    Update repositories.py

--- domains/users/repositories.py
+++ domains/users/repositories.py
@@ -6,18 +6,16 @@ class UserRepository:
         self._session = session
 
     async def get_user(self, *, user_id: int):
-        async with self._session() as session:
-            result = await session.execute(
-                select(UserModel).filter(UserModel.id == user_id)
+        async with self._session() as session:  # Create session properly
+            return (
+                await session.query(UserModel)
+                .filter(UserModel.id == user_id)
+                .first()
             )
-            return result.scalar_one_or_none()
 
-    async def create_user(self, *, user_name: str, password: str):
-        async with self._session() as session:
-            user_entity = UserModel(
-                name=user_name,
-                password=password,
-            )
+    async def create_user(self, *, user_name: str, user_pw: str):
+        async with self._session() as session:  # Create session properly
+            user_entity = UserModel(name=user_name, password=user_pw)
             session.add(user_entity)
             await session.commit()
             return user_entity.id
