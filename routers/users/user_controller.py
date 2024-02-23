b1a6b7894ddf1b74421eacebecd7d72d9a4ba0c0
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-23 22:36:43 +0900

    Update user_controller.py

--- routers/users/user_controller.py
+++ routers/users/user_controller.py
@@ -54,18 +54,18 @@ async def apiawake(db=Depends(provide_session)):
     user_service = UserService(user_repository=UserRepository(session=db))
     user_info = await user_service.get_user(user_id=3321)
     
-   if user_info:
-       return UserItemGetResponse(
-        data=UserItemGetResponse.DTO(
-            id=user_info.id,
-            name=user_info.name,
-            flavor_genre_first=user_info.flavor_genre_first or "",  # If None, set to empty string
-            flavor_genre_second=user_info.flavor_genre_second or "",  # If None, set to empty string
-            flavor_genre_third=user_info.flavor_genre_third or "",  # If None, set to empty string
-            created_at=str(user_info.created_at),  # Convert to string
-            updated_at=str(user_info.updated_at),  # Convert to string
-        )
-       ).dict()
+    if user_info:
+        return UserItemGetResponse(
+            data=UserItemGetResponse.DTO(
+                id=user_info.id,
+                name=user_info.name,
+                flavor_genre_first=user_info.flavor_genre_first or "",  # If None, set to empty string
+                flavor_genre_second=user_info.flavor_genre_second or "",  # If None, set to empty string
+                flavor_genre_third=user_info.flavor_genre_third or "",  # If None, set to empty string
+                created_at=str(user_info.created_at),  # Convert to string
+                updated_at=str(user_info.updated_at),  # Convert to string
+            )
+        ).dict()
     else:
         return {"message": "User not found"}  # Or any appropriate response
 
@@ -76,4 +76,3 @@ async def apiTest():
 @router.get("/") 
 async def apiTest():
     return {"hello":"world"};
-
