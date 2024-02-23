2e0728809e393f54c7e08b3cf94c23aa27c0754b
parkjusu3321 <jackpot3016@naver.com>
2024-02-24 01:17:55 +0900

    123

--- routers/users/user_controller.py
+++ routers/users/user_controller.py
@@ -9,6 +9,11 @@ from domains.users.dto import (
     UserPostResponse,
 )
 
+from domains.users.dto import GameResultItemGetResponse
+from domains.users.services import GameResultService
+from domains.users.repositories import GameResultRepository
+
+
 name = "users"
 router = APIRouter()
 
@@ -27,35 +32,34 @@ async def create(
     return UserPostResponse(id=user_id).dict()
 
 
-@router.get(f'/{name}/{{user_id}}')
-async def get(user_id: int, db=Depends(provide_session)):
-    user_service = UserService(user_repository=UserRepository(session=db))
-    user_info = await user_service.get_user(user_id=user_id)
+@router.get(f'/{name}/{"all"}')
+async def get(order_data: int, db=Depends(provide_session)):
+    gameresult_service = GameResultService(game_result_repository=GameResultRepository(session=db))
+    game_result_info = await gameresult_service.get_game_result(order_data=order_data)
     
-    if user_info:
-        return UserItemGetResponse(
-            data=UserItemGetResponse.DTO(
-                id=user_info.id,
-                name=user_info.name,
-                flavor_genre_first=user_info.flavor_genre_first or "",  # If None, set to empty string
-                flavor_genre_second=user_info.flavor_genre_second or "",  # If None, set to empty string
-                flavor_genre_third=user_info.flavor_genre_third or "",  # If None, set to empty string
-                created_at=str(user_info.created_at),  # Convert to string
-                updated_at=str(user_info.updated_at),  # Convert to string
+    if game_result_info:
+        return GameResultItemGetResponse(
+            data=GameResultItemGetResponse.DTO(
+                order_data=game_result_info.order_data,
+                game_result_player_id=game_result_info.game_result_player_id,
+                game_result_music_id=game_result_info.game_result_music_id,
+                game_result_score=game_result_info.game_result_score,
+                game_result_created_time=str(game_result_info.game_result_created_time),  # Convert to string
             )
         ).dict()
     else:
-        return {"message": "User not found"}  # Or any appropriate response
+        return {"message": "게임 결과를 찾을 수 없습니다."}  # Or any appropriate response
+
 
 
 @router.get("/apiawake")  # Renamed the endpoint for clarity
 async def apiawake(db=Depends(provide_session)):
-    user_service = UserService(user_repository=UserRepository(session=db))
+    user_service = GameResultService(user_repository=UserRepository(session=db))
     user_info = await user_service.get_user(user_id=3321)
     
     if user_info:
-        return UserItemGetResponse(
-            data=UserItemGetResponse.DTO(
+        return GameResultItemGetResponse(
+            data=GameResultItemGetResponse.DTO(
                 id=user_info.id,
                 name=user_info.name,
                 flavor_genre_first=user_info.flavor_genre_first or "",  # If None, set to empty string
@@ -75,3 +79,24 @@ async def apiTest():
 @router.get("/") 
 async def apiTest():
     return {"hello":"world"}
+
+
+@router.get("/plz") 
+async def plz(db=Depends(provide_session)):
+    user_service = UserService(user_repository=UserRepository(session=db))
+    user_info = await user_service.get_user(user_id=3321)
+    
+    if user_info:
+        return UserItemGetResponse(
+            data=UserItemGetResponse.DTO(
+                id=user_info.id,
+                name=user_info.name,
+                flavor_genre_first=user_info.flavor_genre_first or "",  # If None, set to empty string
+                flavor_genre_second=user_info.flavor_genre_second or "",  # If None, set to empty string
+                flavor_genre_third=user_info.flavor_genre_third or "",  # If None, set to empty string
+                created_at=str(user_info.created_at),  # Convert to string
+                updated_at=str(user_info.updated_at),  # Convert to string
+            )
+        ).dict()
+    else:
+        return {"message": "User not found"}  # Or any appropriate response
