83714f33a7f63b2e765e2049c17976331e63beb8
parkjusu3321 <jackpot3016@naver.com>
2024-02-24 15:41:04 +0900

    23

--- dependencies/config.py
+++ dependencies/config.py
@@ -2,11 +2,7 @@ import os
 from functools import lru_cache
 from pydantic_settings import BaseSettings
 
-jwt_secret_key: str = os.getenv(
-        "JWT_SECRET_KEY",
-        "5c2fea6305c8c209714e73b265958703e65c4b40dec4c388dddac06f3f791ec7",
-    )
-jwt_expire_minutes: int = os.getenv("JWT_TOKEN_EXPIRE_MINUTES", 600)
+
 
 class DefaultConfig(BaseSettings):
     postgresql_endpoint: str = os.getenv("POSTGRESQL_ENDPOINT", "svc.sel4.cloudtype.app")
@@ -14,6 +10,12 @@ class DefaultConfig(BaseSettings):
     postgresql_table: str = os.getenv("POSTGRESQL_TABLE", "reaction_db")
     postgresql_user: str = os.getenv("POSTGRESQL_USER", "")
     postgresql_password: int = os.getenv("POSTGRESQL_PASSWORD", "")
+
+    jwt_secret_key: str = os.getenv(
+        "JWT_SECRET_KEY",
+        "5c2fea6305c8c209714e73b265958703e65c4b40dec4c388dddac06f3f791ec7",
+    )
+    jwt_expire_minutes: int = os.getenv("JWT_TOKEN_EXPIRE_MINUTES", 600)
 
 @lru_cache
 def get_config():
